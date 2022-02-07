0e4ac6ff4e15aa971f24385ac832c2f2ce5503bc
Grace Palacz <gracelizbuckley@gmail.com>
2022-02-07 15:23:42 +0000

    tidied up

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
 <?php
 
-//save the conn in a variable
 $db = new PDO('mysql:host=db; dbname=grace-collection', 'root', 'password');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 $query = $db->prepare("SELECT `name`, `image`, `instagram`, `location`, `website` FROM `illustrators`;");
-//run the query against the db
 $query->execute();
 
-//grab the results from the query that has been run
 $result = $query->fetchAll();
 
 echo '<pre>';
