0ec876d342503d7d65fc245c4ea2f98bbc34e3d8
Grace Palacz <gracelizbuckley@gmail.com>
2022-02-07 16:16:58 +0000

    made first few lines into functions

--- functions.php
+++ functions.php
@@ -1,1 +1,14 @@
 <?php
+
+function fetchDatabase(){
+    return new PDO('mysql:host=db; dbname=grace-collection', 'root', 'password');
+}
+
+function queryDatabase($db){
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $query = $db->prepare("SELECT `name`, `image`, `instagram`, `location`, `website` FROM `illustrators`;");
+    $query->execute();
+
+    return $query->fetchAll();
+
+}
