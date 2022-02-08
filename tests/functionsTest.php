664dd29ef996843a31f198d7d727b23985764006
Grace Palacz <gracelizbuckley@gmail.com>
2022-02-08 12:25:41 +0000

    amended name of functions

--- tests/functionsTest.php
+++ tests/functionsTest.php
@@ -18,20 +18,20 @@ class functionsTest extends TestCase {
             ]
         ];
         $expected = '<div>Malika Favre</div><div><img src="test.jpg"alt="Example image of illustrator\'s work"/></div><div>@malikafavre</div><div>Barcelona</div><div>malikafavre.com</div>';
-        $case = loopsthroughArray($input);
+        $case = displayIllustrators($input);
         $this->assertEquals($case, $expected);
     }
     public function testFailureDisplayIllustratorsMissingKey(): void
     {
         $input = [];
         $expected = '<div>No items found in the database</div>';
-        $case = loopsthroughArray($input);
+        $case = displayIllustrators($input);
         $this->assertEquals($case, $expected);
     }
     public function testMalformedDisplayIllustrators()
     {
         $input = true;
         $this->expectException(TypeError::class);
-        loopsthroughArray($input);
+        displayIllustrators($input);
     }
 }
