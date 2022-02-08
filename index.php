e58c4a717f1c3cce3716551884d059b427855f29
Grace Palacz <gracelizbuckley@gmail.com>
2022-02-08 13:34:49 +0000

    added stylesheet file and set up html in index

--- index.php
+++ index.php
@@ -8,3 +8,20 @@ $result = queryDatabase($db);
 $result = displayIllustrators($result);
 
 echo $result;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+	<title>Title</title>
+	<link rel="shortcut icon" type="image/jpg" href="FAVICON FILE NAME HERE"/>
+	<link href= "normalize.css" type="text/css" rel="stylesheet"/>
+	<meta name="viewport" content="width=device-width">
+	<link href="style.css" rel="stylesheet" type="text/css" />
+  </head>
+    <body>
+
+	</body>
+
+</html>
