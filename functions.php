dbd0b2b6e8aee2268d3f24696c95d640590775e4
Grace Palacz <gracelizbuckley@gmail.com>
2022-02-08 10:05:06 +0000

    added images

--- functions.php
+++ functions.php
@@ -1,14 +1,38 @@
 <?php
 
+/**
+ * Fetches a database
+ *
+ * @return PDO which is a link to the database
+ */
 function fetchDatabase(){
     return new PDO('mysql:host=db; dbname=grace-collection', 'root', 'password');
 }
 
+
+/**
+ * Retrieves all data from illustrators database
+ *
+ * @param $db database to be queried
+ *
+ * @return mixed the result of the query
+ */
 function queryDatabase($db){
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     $query = $db->prepare("SELECT `name`, `image`, `instagram`, `location`, `website` FROM `illustrators`;");
     $query->execute();
-
     return $query->fetchAll();
-
 }
+
+function loopsthroughArray (array $illustrators): string {
+    $result = '';
+    foreach ($illustrators as $illustrator){
+        $result .= '<div>' . $illustrator['name'] . '</div>';
+        $result .= '<div><img src="' . $illustrator['image'] . '"' . 'alt="Example image of illustrator\'s work"/>' . '</div>';
+        $result .= '<div>' . $illustrator['instagram'] . '</div>';
+        $result .= '<div>' . $illustrator['location'] . '</div>';
+        $result .= '<div>' . $illustrator['website'] . '</div>';
+    }
+    return $result;
+}
+
