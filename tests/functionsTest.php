ed1ca10fa9810f48abf8a8ee135cb7990bda480b
Grace Palacz <gracelizbuckley@gmail.com>
2022-02-09 10:20:17 +0000

    amended functions

--- tests/functionsTest.php
+++ tests/functionsTest.php
@@ -17,14 +17,14 @@ class functionsTest extends TestCase {
                 'location' => 'Barcelona'
             ]
         ];
-        $expected = '<main><section><h2>Malika Favre</h2><div><img src="test.jpg"/></div><h3>@malikafavre</h3><p>malikafavre.com</p><p>Barcelona</p></section></main>';
+        $expected = '<main><section><h2>Malika Favre</h2><div><img src="test.jpg"alt="Example image of illustrator\'s work"/></div><h3>@malikafavre</h3><p>malikafavre.com</p><p>Barcelona</p></section></main>';
         $case = displayIllustrators($input);
         $this->assertEquals($case, $expected);
     }
     public function testFailureDisplayIllustratorsMissingKey(): void
     {
         $input = [];
-        $expected = '<div>No items found in the database</div>';
+        $expected = '<div>Error! No items found in the database</div>';
         $case = displayIllustrators($input);
         $this->assertEquals($case, $expected);
     }
